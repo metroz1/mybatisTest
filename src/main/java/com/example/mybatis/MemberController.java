4162ceddca790d15afbb4af8665845c2a622d3d5
metroz1 <wlstjr132@gmail.com>
2023-05-25 08:00:56 +0900

    Member Select Test complete

--- src/main/java/com/example/mybatis/MemberController.java
+++ src/main/java/com/example/mybatis/MemberController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +19,11 @@ public class MemberController {
 
         return memberService.findAll();
     }
-//
-//    @GetMapping("/findOne")
-//    publi
+
+    @GetMapping("/findOne")
+    public ResponseEntity<?> findMember(@RequestParam("name") String name) {
+
+        return memberService.findOne(name);
+    }
 
 }
